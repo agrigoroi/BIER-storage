19bfded617e6d64d165bd60feb7bf8f621bf50c8
Alexandru Grigoroi <alexandru@grigoroi.com>
2014-03-17 16:45:27 +0000

    Updated the default transport config

--- node.js
+++ node.js
@@ -5,16 +5,7 @@ var config = {
   bootstraps : ["127.0.0.1:3000", "127.0.0.1:3001", "127.0.0.1:3002"],
   reactor : {
     protocol  : 'jsonrpc2',
-    type      : 'SimUDP',
-    transport : {
-      transports : [
-        'flashsocket', 
-        'htmlfile', 
-        'xhr-multipart', 
-        'xhr-polling', 
-        'jsonp-polling'
-      ]
-    }
+    type      : 'SimUDP',    
   }
 };
 
